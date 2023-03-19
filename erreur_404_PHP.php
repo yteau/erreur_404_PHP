2080e68fce05077b8a65ab85167c1cb736f05ba2
itto <121813459+yteau@users.noreply.github.com>
2023-03-19 17:01:17 +0100

    Add files via upload

---
+++ erreur_404_PHP.php
@@ -0,0 +1,10 @@
+<?php
+// Définit le code de réponse à 404
+header("HTTP/1.0 404 Not Found");
+
+// Affiche le message d'erreur 404
+echo "Page not found";
+?>
+
+// Pour générer une erreur 404 en PHP, vous pouvez utiliser la fonction header() pour renvoyer une en-tête HTTP 404 Not Found au client. Voici un exemple de code:
+Ce code définit le code de réponse à 404 et envoie une page de réponse contenant le message "Page not found". 
